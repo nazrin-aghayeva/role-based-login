e977c8bf780fb5b05bed71d4399beebd19a30dba
nazrin-aghayeva <nazrinaghayeva@gmail.com>
2020-10-28 17:02:36 +0400

    -- resolved

--- src/main/java/com/employee/app/App.java
+++ src/main/java/com/employee/app/App.java
@@ -1,14 +1,15 @@
 package com.employee.app;
 
+import com.employee.app.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.employee.app.entities"})
-
 public class App {
     public static void main(String[] args) { SpringApplication.run(App.class, args);}
 }
